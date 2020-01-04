19defa3832dea62934cb6576ac874b4ab13304e9
RaulEK <kattai.raul@gmail.com>
2020-01-04 13:49:25 +0200

    Updates

---
+++ precache-manifest.2814371b5f6142218068cc016dca2e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb79ade25274976d2f5a4ed1c35355f",
+    "url": "/os-hw4/index.html"
+  },
+  {
+    "revision": "db47bf695c1b8872d598",
+    "url": "/os-hw4/static/css/main.d742b968.chunk.css"
+  },
+  {
+    "revision": "a3a0efc8782d7daf58aa",
+    "url": "/os-hw4/static/js/2.e4f41027.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/os-hw4/static/js/2.e4f41027.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db47bf695c1b8872d598",
+    "url": "/os-hw4/static/js/main.d315b2de.chunk.js"
+  },
+  {
+    "revision": "07cde0f1beeb2f587b60",
+    "url": "/os-hw4/static/js/runtime-main.462b2f11.js"
+  }
+]);
